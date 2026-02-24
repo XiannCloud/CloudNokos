b8e66ac1d8b4e29c853e1c5d47a0d31a0ac66770
XiannCloud <empiijasaboostsosmed@gmail.com>
2026-02-24 13:24:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ process.on("unhandledRejection", (reason) => console.log("[ANTI CRASH] Unhandled
 process.on("uncaughtException", (err) => console.log("[ANTI CRASH] Uncaught Exception:", err));
 process.on("uncaughtExceptionMonitor", (err) => console.log("[ANTI CRASH MONITOR]:", err));
 
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Bot is Online!'));
+app.listen(process.env.PORT || 3000, () => console.log('Web server ready'));
+
 const config = require("./config.js");
 const TelegramBot = require("node-telegram-bot-api");
 const moment = require('moment-timezone');
